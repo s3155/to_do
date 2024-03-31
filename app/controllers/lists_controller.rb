bb401cd49f21b4aa573fe0e74e4f3993d881d62c
s3155 <yutaka.7sn@gmail.com>
2024-04-01 00:39:42 +0900

    lists_controllerの変更

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,40 +1,49 @@
 class ListsController < ApplicationController
-    def new
-      @list = List.new
-    end
-    def create
-        @list = List.new(params[:list])
-        @list.save
-        redirect_to list_path(@liset)
-    end
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
 
-    def list_params
-      params.require(:list).permit(:name, :description)
-    end
+  def new
+    @list = List.new
+  end
 
-    def index
-        @lists = List.all
-    end
-    def show
-      @list = List.find(list_params)
-    end
-  
-    def edit
-      @list = List.find(params[:id])
-    end
-
-    def update
-      @list = list.find(params[:id])
-       if @list.update(list_params)
-           redirect_to lists_path
-        else
-           redirect_to edit_list_path
-        end
-    end
-
-    def destroy
-      @list = List.find(params[:id])
-      @list.destroy
-      redirect_to lists_path
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render 'new'
     end
   end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :description)
+  end
+end
