c88b68739a88a2bdfd017ad53b739dd84205f265
s3155 <yutaka.7sn@gmail.com>
2024-04-30 12:20:33 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     omniauth_callbacks: "omniauth_callbacks"
   }
   #get 'homes/index'
- root to: redirect('/users/sign_in') # ログインページにリダイレクトする
-  #root 'homes#index'
+ #root to: redirect('/users/sign_in') # ログインページにリダイレクトする
+  root 'lists#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
